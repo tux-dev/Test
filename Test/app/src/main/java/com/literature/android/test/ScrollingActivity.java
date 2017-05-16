4d874d1b3e5f9d8d631a07f80ed698215293fe1b
tux-dev <www.mher777@mail.ru>
2017-05-16 07:35:47 +0400

    Test commit 'Hellow World'

--- Test/app/src/main/java/com/literature/android/test/ScrollingActivity.java
+++ Test/app/src/main/java/com/literature/android/test/ScrollingActivity.java
@@ -5,9 +5,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class ScrollingActivity extends AppCompatActivity {
 
@@ -26,6 +27,8 @@ public class ScrollingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Toast.makeText(this, "Hello World !", Toast.LENGTH_LONG).show();
     }
 
     @Override
