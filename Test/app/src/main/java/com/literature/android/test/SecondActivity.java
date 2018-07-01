3ab1dd887ead50ea81eb3d4903da68910864fb1d
tux-dev <www.mher777@mail.ru>
2018-07-01 13:57:45 +0300

    Demonstrated the Persistent BottomSheet

--- Test/app/src/main/java/com/literature/android/test/SecondActivity.java
+++ Test/app/src/main/java/com/literature/android/test/SecondActivity.java
@@ -1,13 +1,18 @@
 package com.literature.android.test;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private BottomSheetBehavior bottomSheet;
+    private ImageView action;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +20,67 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        action = (ImageView) findViewById(R.id.imgview_action);
+        action.setOnClickListener(this);
+        View view = findViewById(R.id.bottom_sheet);
+        bottomSheet = BottomSheetBehavior.from(view);
+        bottomSheet.setPeekHeight(220);
+        bottomSheet.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                Log.v("test", "state is changed");
+                switch (newState) {
+
+                    case BottomSheetBehavior.STATE_COLLAPSED: {
+                        Log.v("test", "collapsed");
+                    }
+                    case BottomSheetBehavior.STATE_SETTLING: {
+
+                        Log.v("test", "settling");
+                    }
+                    case BottomSheetBehavior.STATE_EXPANDED: {
+                        Log.v("test", "expanded");
+                    }
+                    case BottomSheetBehavior.STATE_HIDDEN: {
+
+                        Log.v("test", "hidden");
+                    }
+                    case BottomSheetBehavior.STATE_DRAGGING: {
+
+                        Log.v("test", "dragging");
+                    }
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                Log.v("test", "onSlide");
             }
         });
     }
 
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.imgview_action:
+                if (bottomSheet == null) {
+                    return;
+                }
+
+                if (bottomSheet.getState() == BottomSheetBehavior.STATE_COLLAPSED) {
+                    setBottomSheetState(BottomSheetBehavior.STATE_EXPANDED);
+                    action.setImageResource(android.R.drawable.arrow_down_float);
+
+                } else {
+                    setBottomSheetState(BottomSheetBehavior.STATE_COLLAPSED);
+                    action.setImageResource(android.R.drawable.arrow_up_float);
+                }
+                break;
+        }
+    }
+
+    private void setBottomSheetState(int state) {
+        bottomSheet.setState(state);
+    }
 }
