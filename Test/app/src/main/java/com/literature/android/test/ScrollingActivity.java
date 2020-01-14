3788294015a900d3412f3c5cf572763e0181afe3
Mher Grigoryan <m.grigoryan@bslab.ru>
2020-01-14 15:10:43 +0300

    Migrated with androidx

--- Test/app/src/main/java/com/literature/android/test/ScrollingActivity.java
+++ Test/app/src/main/java/com/literature/android/test/ScrollingActivity.java
@@ -4,17 +4,14 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
-import java.util.ArrayList;
-import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class ScrollingActivity extends AppCompatActivity implements Application.ActivityLifecycleCallbacks {
 
